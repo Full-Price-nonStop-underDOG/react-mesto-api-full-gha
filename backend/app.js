e741da34d2f488c7726e871207193cc5413fc82a
full-price-nonstop-underdog <korvin.korp@yandex.ru>
2023-09-08 22:54:09 +0300

    -next

--- backend/app.js
+++ backend/app.js
@@ -129,7 +129,7 @@ app.use((err, req, res, next) => {
 });
 
 // Обработка ошибок и отправка ответа
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   const statusCode = err.statusCode || 500;
   const message =
     statusCode === 500
